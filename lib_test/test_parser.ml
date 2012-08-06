48a22dfc935f8d52cfbd824d5fd0109f494e58a8
Anil Madhavapeddy <anil@recoil.org>
2012-08-06 23:19:32 +0100

    add a test case for several HTTP modes

---
+++ lib_test/test_parser.ml
@@ -0,0 +1,189 @@
+(*
+ * Copyright (c) 2012 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+
+open OUnit
+open Printf
+
+let basic_req =
+"GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n"
+
+let basic_res =
+"HTTP/1.1 200 OK
+Date: Mon, 23 May 2005 22:38:34 GMT
+Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)
+Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT
+Etag: \"3f80f-1b6-3e1cb03b\"
+Accept-Ranges:  none
+Content-Length: 0
+Connection: close
+Content-Type: text/html; charset=UTF-8"
+
+let post_req =
+"POST /path/script.cgi HTTP/1.0
+From: frog@jmarshall.com
+User-Agent: HTTPTool/1.0
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 32
+
+home=Cosby&favorite+flavor=flies"
+
+let post_data_req =
+"POST /path/script.cgi HTTP/1.0
+From: frog@jmarshall.com
+User-Agent: HTTPTool/1.0
+Content-Length: 32
+
+home=Cosby&favorite+flavor=flies"
+
+let post_chunked_req =
+"POST /foo HTTP/1.1
+Date: Fri, 31 Dec 1999 23:59:59 GMT
+Content-Type: text/plain
+Transfer-Encoding: chunked
+
+1a; ignore-stuff-here
+abcdefghijklmnopqrstuvwxyz
+10
+1234567890abcdef
+0
+some-footer: some-value
+another-footer: another-value
+
+"
+
+let chunked_res =
+"HTTP/1.1 200 OK
+Date: Fri, 31 Dec 1999 23:59:59 GMT
+Content-Type: text/plain
+Transfer-Encoding: chunked
+
+1a; ignore-stuff-here
+abcdefghijklmnopqrstuvwxyz
+10
+1234567890abcdef
+0
+some-footer: some-value
+another-footer: another-value
+
+"
+
+let basic_res_plus_crlf = basic_res ^ "\r\n\r\n"
+
+let basic_req_parse () =
+  let open Cohttp in
+  let open IO.M in
+  let ic = IO.ic_of_buffer (Lwt_bytes.of_string basic_req) in
+  Parser.parse_request_fst_line ic >>=
+  function
+  |Some (meth, uri, ver) ->
+    assert_equal ver `HTTP_1_1;
+    assert_equal meth `GET;
+    assert_equal (Uri.to_string uri) "/index.html";
+    return ()
+  |None -> assert false
+
+let basic_res_parse res () =
+  let open Cohttp in
+  let open IO.M in
+  let ic = IO.ic_of_buffer (Lwt_bytes.of_string res) in
+  Parser.parse_response_fst_line ic >>=
+  function
+  |Some (version, status) ->
+     (* Parse first line *)
+     assert_equal version `HTTP_1_1;
+     assert_equal status `OK;
+     (* Now parse the headers *)
+     Parser.parse_headers ic >>= fun headers ->
+     assert_equal (List.assoc "connection" headers) "close";
+     assert_equal (List.assoc "accept-ranges" headers) "none";
+     assert_equal (List.assoc "content-type" headers) "text/html; charset=UTF-8";
+     return ()
+  |None -> assert false
+
+let req_parse () =
+  let open Cohttp in
+  let open IO.M in
+  let ic = IO.ic_of_buffer (Lwt_bytes.of_string basic_req) in
+  Request.parse ic >>= function
+  |None -> assert false
+  |Some req ->
+    assert_equal `GET (Request.meth req);
+    assert_equal "/index.html" (Request.path req);
+    assert_equal `HTTP_1_1 (Request.version req);
+    return ()
+
+let post_form_parse () =
+  let open Cohttp in
+  let open IO.M in
+  let ic = IO.ic_of_buffer (Lwt_bytes.of_string post_req) in
+  Request.parse ic >>= function
+  |None -> assert false
+  |Some req ->
+    assert_equal (Some "Cosby") (Request.param "home" req);
+    assert_equal (Some "flies") (Request.param "favorite flavor" req);
+    assert_equal None (Request.param "nonexistent" req);
+    return ()
+
+let post_data_parse () =
+  let open Cohttp in
+  let open IO.M in
+  let ic = IO.ic_of_buffer (Lwt_bytes.of_string post_data_req) in
+  Request.parse ic >>= function
+  |None -> assert false
+  |Some req ->
+    let data = () in (* TODO 
+    assert_equal data "home=Cosby&favorite+flavor=flies"; *)
+    return ()
+
+let post_chunked_parse () =
+  let open Cohttp in
+  let open IO.M in
+  let ic = IO.ic_of_buffer (Lwt_bytes.of_string post_chunked_req) in
+  Request.parse ic >>= function
+  |None -> assert false
+  |Some req ->
+    let data = () in (* TODO *)
+    return ()
+
+let test_cases =
+  let tests = [ basic_req_parse; req_parse; post_form_parse; post_data_parse; 
+    post_chunked_parse; (basic_res_parse basic_res); (basic_res_parse basic_res_plus_crlf) ] in
+  List.map (fun x -> "test" >:: (fun () -> Lwt_unix.run (x ()))) tests
+
+(* Returns true if the result list contains successes only.
+   Copied from oUnit source as it isnt exposed by the mli *)
+let rec was_successful =
+  function
+    | [] -> true
+    | RSuccess _::t
+    | RSkip _::t ->
+        was_successful t
+    | RFailure _::_
+    | RError _::_
+    | RTodo _::_ ->
+        false
+
+let _ =
+  let suite = "Parser" >::: test_cases in
+  let verbose = ref false in
+  let set_verbose _ = verbose := true in
+  Arg.parse
+    [("-verbose", Arg.Unit set_verbose, "Run the test in verbose mode.");]
+    (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
+    ("Usage: " ^ Sys.argv.(0) ^ " [-verbose]");
+  if not (was_successful (run_test_tt ~verbose:!verbose suite)) then
+  exit 1
