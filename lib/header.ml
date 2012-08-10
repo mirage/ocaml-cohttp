e286d58e71215c403c6e2617e38f94be9eb604d1
Anil Madhavapeddy <anil@recoil.org>
2012-08-10 11:43:42 -0700

    add a Header module to abstract the representation of headers and parameters

---
+++ lib/header.ml
@@ -0,0 +1,27 @@
+(*
+ * Copyright (c) 2012 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+
+type t = (string, string) Hashtbl.t
+
+let init () = Hashtbl.create 7
+let add h k v = Hashtbl.add h k v
+let remove h k = Hashtbl.remove h k
+let get h k = Hashtbl.find_all h k
+let of_list l =
+  let h = init () in
+  List.iter (fun (k,v) -> add h k v) l;
+  h
