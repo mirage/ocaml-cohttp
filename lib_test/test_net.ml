04ec587ec7fa9e72abd1bcefaccbd1744a89c2d4
Anil Madhavapeddy <anil@recoil.org>
2012-08-11 22:27:25 -0700

    add an external-network-using test, initially just simple client fetches

---
+++ lib_test/test_net.ml
@@ -0,0 +1,55 @@
+(*
+ * Copyright (c) 2012 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+
+open OUnit
+open Printf
+
+open Lwt
+
+let make_net_req () =
+  lwt _ = Cohttp_lwt.call `GET (Uri.of_string "http://anil.recoil.org/") in
+  lwt _ = Cohttp_lwt.call `GET (Uri.of_string "https://github.com/") in
+  return ()
+
+let test_cases =
+  let tests = [ make_net_req;
+  ] in
+  List.map (fun x -> "test" >:: (fun () -> Lwt_unix.run (x ()))) tests
+
+(* Returns true if the result list contains successes only.
+   Copied from oUnit source as it isnt exposed by the mli *)
+let rec was_successful =
+  function
+    | [] -> true
+    | RSuccess _::t
+    | RSkip _::t ->
+        was_successful t
+    | RFailure _::_
+    | RError _::_
+    | RTodo _::_ ->
+        false
+
+let _ =
+  let suite = "Parser" >::: test_cases in
+  let verbose = ref false in
+  let set_verbose _ = verbose := true in
+  Arg.parse
+    [("-verbose", Arg.Unit set_verbose, "Run the test in verbose mode.");]
+    (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
+    ("Usage: " ^ Sys.argv.(0) ^ " [-verbose]");
+  if not (was_successful (run_test_tt ~verbose:!verbose suite)) then
+  exit 1
