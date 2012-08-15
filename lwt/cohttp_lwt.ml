6abd5cffd6b4937718d10d0b6c557cc0690934be
Anil Madhavapeddy <anil@recoil.org>
2012-08-15 16:53:34 -0700

    greatly simplify the Lwt server backend by using streams to map requests and responses, with barriers in place to ensure that bodys are not corrupted.
    this requires Lwt >= 2.4.1 due to the use of Lwt_stream.on_terminate

--- lwt/cohttp_lwt.ml
+++ lwt/cohttp_lwt.ml
@@ -46,224 +46,152 @@ module Response = Response.M(IO)
 
 open Lwt
 
-let build_sockaddr (addr, port) =
-  try_lwt
-    (* should this be lwt hent = Lwt_lib.gethostbyname addr ? *)
-    let hent = Unix.gethostbyname addr in
-    return (Unix.ADDR_INET (hent.Unix.h_addr_list.(0), port))
-  with _ -> 
-    raise_lwt (Failure ("cant resolve hostname: " ^ addr))
+let stream_of_body read_fn ic =
+  let fin = ref false in
+  Lwt_stream.from (fun () ->
+    match !fin with
+    |true -> return None
+    |false -> begin
+      match_lwt read_fn ic with
+      |Transfer.Done -> 
+        return None
+      |Transfer.Final_chunk c ->
+        fin := true;
+        return (Some c);
+      |Transfer.Chunk c ->
+        return (Some c)
+    end
+  )
 
-let host_of_uri uri = 
-  match Uri.host uri with
-  |None -> "localhost"
-  |Some h -> h
+let wait_for_stream st =
+  let t,u = task () in
+  Lwt_stream.on_terminate st (wakeup u);
+  t
 
-let port_of_uri uri =
-  match Uri.port uri with
-  |None -> begin
-     match Uri.scheme uri with 
-     |Some "https" -> 443 (* TODO: actually support https *)
-     |Some "http" | Some _ |None -> 80
-  end
-  |Some p -> p
-
-module Normal = struct
-
-  let connect uri iofn =
-    let open Uri in
-    let address = host_of_uri uri in
-    let port = port_of_uri uri in
-    lwt sockaddr = build_sockaddr (address, port) in
-    Lwt_io.with_connection sockaddr iofn
-
-end
-
-module SSL = struct
-
-  let sslcontext =
-    Ssl.init ();
-    Ssl.create_context Ssl.SSLv23 Ssl.Client_context
-
-  let connect uri iofn =
-    let address = host_of_uri uri in
-    let port = port_of_uri uri in
-    lwt sockaddr = build_sockaddr (address, port) in
-    let fd = Lwt_unix.socket (Unix.domain_of_sockaddr sockaddr) Unix.SOCK_STREAM 0 in
-    lwt () = Lwt_unix.connect fd sockaddr in
-    lwt sock = Lwt_ssl.ssl_connect fd sslcontext in
-    let ic = Lwt_ssl.in_channel_of_descr sock in
-    let oc = Lwt_ssl.out_channel_of_descr sock in
-    try_lwt
-      lwt res = iofn (ic,oc) in
-      Lwt_ssl.close sock >>
-      return res
-    with exn ->
-      Lwt_ssl.close sock >>
-      fail exn
-end
-
-let connect uri iofn =
-  match Uri.scheme uri with
-  |Some "https" -> SSL.connect uri iofn
-  |Some "http" -> Normal.connect uri iofn
-  |Some _ | None -> fail (Failure "unknown scheme")
- 
 module Client = struct
 
-  type response = (Response.response * string Lwt_stream.t) option
+  type response = (Response.response * string Lwt_stream.t option) option
 
+  let write_request ?body req oc =
+    Request.write (fun req oc ->
+      match body with
+      |None -> return ()
+      |Some b -> Lwt_stream.iter_s (fun c -> Request.write_body c req oc) b
+    ) req oc
+
+  let read_response ?(close=false) ic =
+    match_lwt Response.read ic with
+    |None -> return None
+    |Some res -> begin
+      match Response.has_body res with
+      |true ->
+        let body = stream_of_body (Response.read_body res) ic in
+        if close then
+          Lwt_stream.on_terminate body (fun () -> Cohttp_lwt_net.close_in ic);
+        return (Some (res, Some body))
+      |false ->
+        return (Some (res, None))
+    end
+ 
   let call ?headers ?body meth uri =
-    let headers =
-      match headers with
-      |None -> Header.init ()
-      |Some h -> h in
     let encoding = 
       match body with 
       |None -> Transfer.Fixed 0L 
       |Some _ -> Transfer.Chunked in
-    let req = Request.make ~meth ~encoding headers uri in
-    let mvar = Lwt_mvar.create_empty () in
-    let _ = connect uri
-      (fun (ic, oc) ->
-        lwt () = Request.write
-          (match body with
-           |None -> (fun _ _ -> return ())
-           |Some bs ->
-             (fun req oc ->
-               Lwt_stream.iter_s (fun b -> Request.write_body b req oc) bs)
-          ) req oc in
-        match_lwt Response.read ic with
-        |None ->
-          Lwt_mvar.put mvar None
-        |Some res ->
-          let body, push_body = Lwt_stream.create () in
-          let rec push_t () =
-            match_lwt Response.read_body res ic with
-            |Transfer.Done -> push_body None; return ()
-            |Transfer.Final_chunk buf -> push_body (Some buf); push_body None; return ()
-            |Transfer.Chunk buf -> push_body (Some buf); push_t ()
-          in
-          Lwt_mvar.put mvar (Some (res, body)) >>= push_t
-      ) in
-    Lwt_mvar.take mvar
+    let req = Request.make ~meth ~encoding ?headers uri in
+    lwt (ic,oc) = Cohttp_lwt_net.connect_uri uri in
+    write_request ?body req oc >>
+    read_response ~close:true ic
 
-   let head ?headers uri = call ?headers `HEAD uri 
-   let get ?headers uri = call ?headers `GET uri 
-   let delete ?headers uri = call ?headers `DELETE uri 
-   let post ?headers ?body uri = call ?headers ?body `POST uri 
-   let put ?headers ?body uri = call ?headers ?body `POST uri 
+  let head ?headers uri = call ?headers `HEAD uri 
+  let get ?headers uri = call ?headers `GET uri 
+  let delete ?headers uri = call ?headers `DELETE uri 
+  let post ?headers ?body uri = call ?headers ?body `POST uri 
+  let put ?headers ?body uri = call ?headers ?body `POST uri 
 
-   let post_form ?headers ~params uri =
-     let headers =
-       match headers with
-       |None -> Header.of_list ["content-type","application/x-www-form-urlencoded"]
-       |Some h -> Header.add h "content-type" "application/x-www-form-urlencoded"
-     in
-     let body = Lwt_stream.of_list [(Uri.encoded_of_query (Header.to_list params))] in
-     post ~headers ~body uri
+  let post_form ?headers ~params uri =
+    let headers =
+      match headers with
+      |None -> Header.of_list ["content-type","application/x-www-form-urlencoded"]
+      |Some h -> Header.add h "content-type" "application/x-www-form-urlencoded"
+    in
+    let body = Lwt_stream.of_list [(Uri.encoded_of_query (Header.to_list params))] in
+    post ~headers ~body uri
+
+  let callv ?(ssl=false) host port reqs =
+    lwt (ic, oc) = Cohttp_lwt_net.connect ~ssl host port in
+    (* Serialise the requests out to the wire *)
+    Lwt_stream.on_terminate reqs (fun () -> Cohttp_lwt_net.close_out oc);
+    let _ = Lwt_stream.iter_s (fun (req,body) -> write_request ?body req oc) reqs in
+    (* Read the responses *)
+    let resps = Lwt_stream.from (fun () -> read_response ic) in
+    Lwt_stream.on_terminate resps (fun () -> Cohttp_lwt_net.close_in ic);
+    return resps
 end
 
 module Server = struct
 
-  let try_close chan =
-    catch (fun () -> Lwt_io.close chan)
-    (function |_ -> return ())
-
-  let init_socket sockaddr =
-    let suck = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-    Lwt_unix.setsockopt suck Unix.SO_REUSEADDR true;
-    Lwt_unix.bind suck sockaddr;
-    Lwt_unix.listen suck 15;
-    suck
-
-  let process_accept ~sockaddr ~timeout callback (client,_) =
-    (* client is now connected *)
-    let inchan = Lwt_io.of_fd Lwt_io.input client in
-    let outchan = Lwt_io.of_fd Lwt_io.output client in
- 
-    let c = callback inchan outchan in
-    let events = match timeout with
-      |None -> [c]
-      |Some t -> [c; (Lwt_unix.sleep (float_of_int t) >> return ()) ] in
-    Lwt.pick events >> try_close outchan >> try_close inchan
-  
-  let tcp_server ~sockaddr ~timeout callback =
-    let suck = init_socket sockaddr in
-    let rec handle_connection () =
-       lwt x = Lwt_unix.accept suck in
-       let _ =  process_accept ~sockaddr ~timeout callback x in
-       handle_connection()
-    in
-    handle_connection ()
-
   type conn_id = int
-  type response = Response.response * string Lwt_stream.t
   let string_of_conn_id = string_of_int
+  type response = Response.response * string Lwt_stream.t option
 
   type config = {
     address: string;
-    callback: conn_id -> Request.request -> 
-      (Response.response * string Lwt_stream.t) Lwt.t;
-    conn_closed : conn_id -> unit;
+    callback: conn_id -> ?body:string Lwt_stream.t -> Request.request -> response Lwt.t;
+    conn_closed : conn_id -> unit -> unit;
     port: int;
     root_dir: string option;
     timeout: int option;
   }
 
   let respond_string ?headers ~status ~body () =
-    let headers = match headers with |None -> Header.init () |Some h -> h in
     let res = Response.make ~status 
-      ~encoding:(Transfer.Fixed (Int64.of_int (String.length body))) headers in
-    let body = Lwt_stream.of_list [body] in
+      ~encoding:(Transfer.Fixed (Int64.of_int (String.length body))) ?headers () in
+    let body = Some (Lwt_stream.of_list [body]) in
     return (res,body)
 
   let respond_error ~status ~body () =
     respond_string ~status ~body:("Error: "^body) ()
 
-  let invoke_callback conn_id (req:Request.request) spec =
-    try_lwt 
-      spec.callback conn_id req
-    with e ->
-      respond_error ~status:`Internal_server_error ~body:(Printexc.to_string e) ()
-
   let daemon_callback spec =
     let conn_id = ref 0 in
     let daemon_callback ic oc =
       let conn_id = incr conn_id; !conn_id in
-      let responses, response_push = Lwt_stream.create () in
-      let response_t =
-         lwt () = Lwt_stream.iter_s (fun (res, body) ->
-             Response.write 
-               (fun res oc -> 
-                  Lwt_stream.iter_s (fun s ->
-                    Response.write_body s res oc) body
-               ) res oc
-         ) responses in
-         try_close oc
-      in
-      let rec read_request () =
+      (* Read the requests *)
+      let req_stream = Lwt_stream.from (fun () ->
         match_lwt Request.read ic with
-        |None -> 
-          response_push None;
-          spec.conn_closed conn_id;
-          return ()
         |Some req -> begin
-          lwt (res,body) = invoke_callback conn_id req spec in
-          response_push (Some (res,body));
-          match Request.header req "connection" with
-          |["close"] -> 
-             response_push None;
-             return ()
-          |_ -> 
-             read_request ()
+          (* Ensure the input body has been fully read before reading again *)
+          match Request.has_body req with
+          |true ->
+            let req_body = stream_of_body (Request.read_body req) ic in
+            wait_for_stream req_body >>= fun () -> return (Some (req, Some req_body))
+          |false -> return (Some (req, None))
         end
-      in
-      response_t <&> (read_request ())
+        |None -> return None
+      ) in
+      (* Map the requests onto a response stream to serialise out *)
+      let res_stream = Lwt_stream.map_s (fun (req, body) -> 
+        try_lwt 
+          spec.callback conn_id ?body req
+        with exn ->
+          respond_error ~status:`Internal_server_error ~body:(Printexc.to_string exn) ()
+      ) req_stream in
+      (* Clean up resources when the response stream terminates and call
+       * the user callback *)
+      Lwt_stream.on_terminate res_stream (spec.conn_closed conn_id);
+      (* Transmit the responses *)
+      Lwt_stream.iter_s (fun (res, body) ->
+        Response.write (fun res oc ->
+          match body with
+          |None -> return ()
+          |Some b -> Lwt_stream.iter_s (fun c -> Response.write_body c res oc) b
+        ) res oc
+      ) res_stream
     in daemon_callback
  
   let main spec =
     let () = match spec.root_dir with Some dir -> Sys.chdir dir | None -> () in
-    lwt sockaddr = build_sockaddr (spec.address, spec.port) in
-    tcp_server ~sockaddr ~timeout:spec.timeout (daemon_callback spec)
+    lwt sockaddr = Cohttp_lwt_net.build_sockaddr spec.address spec.port in
+    Cohttp_lwt_net.Tcp_server.init ~sockaddr ~timeout:spec.timeout (daemon_callback spec)
 end
--- lwt/cohttp_lwt.mli
+++ lwt/cohttp_lwt.mli
@@ -29,7 +29,7 @@ module Request : sig
   val transfer_encoding : request -> string
 
   val make : ?meth:Code.meth -> ?version:Code.version -> 
-    ?encoding:Transfer.encoding -> Header.t -> Uri.t -> request
+    ?encoding:Transfer.encoding -> ?headers:Header.t -> Uri.t -> request
 
   val read : Lwt_io.input_channel -> request option Lwt.t
   val read_body : request -> Lwt_io.input_channel -> Transfer.chunk Lwt.t
@@ -37,7 +37,8 @@ module Request : sig
   val write_header : request -> Lwt_io.output_channel -> unit Lwt.t
   val write_body : string -> request -> Lwt_io.output_channel -> unit Lwt.t
   val write_footer : request -> Lwt_io.output_channel -> unit Lwt.t
-  val write : (request -> Lwt_io.output_channel -> unit Lwt.t) -> request -> Lwt_io.output_channel -> unit Lwt.t
+  val write : (request -> Lwt_io.output_channel -> unit Lwt.t) -> request -> 
+    Lwt_io.output_channel -> unit Lwt.t
 end
 
 module Response : sig
@@ -47,7 +48,7 @@ module Response : sig
   val headers: response -> Header.t
 
   val make : ?version:Code.version -> ?status:Code.status_code -> 
-    ?encoding:Transfer.encoding -> Header.t -> response
+    ?encoding:Transfer.encoding -> ?headers:Header.t -> unit -> response
 
   val read : Lwt_io.input_channel -> response option Lwt.t
   val read_body : response -> Lwt_io.input_channel -> Transfer.chunk Lwt.t
@@ -60,7 +61,7 @@ module Response : sig
 end
 
 module Client : sig
-  type response = (Response.response * string Lwt_stream.t) option
+  type response = (Response.response * string Lwt_stream.t option) option
 
   val call : ?headers:Header.t -> ?body:string Lwt_stream.t -> 
     Code.meth -> Uri.t -> response Lwt.t
@@ -71,27 +72,29 @@ module Client : sig
   val post_form : ?headers:Header.t -> params:Header.t -> Uri.t -> response Lwt.t
   val put : ?headers:Header.t -> ?body:string Lwt_stream.t -> Uri.t -> response Lwt.t
   val delete : ?headers:Header.t -> Uri.t -> response Lwt.t
+
+  val callv : ?ssl:bool -> string -> int ->
+      (Request.request * string Lwt_stream.t option) Lwt_stream.t ->
+      (Response.response * string Lwt_stream.t option) Lwt_stream.t Lwt.t
 end
 
 module Server : sig
   type conn_id
-  type response = Response.response * string Lwt_stream.t
+  type response = Response.response * string Lwt_stream.t option
 
   val string_of_conn_id : conn_id -> string
 
   type config = {
     address : string;
-    callback : conn_id -> Request.request -> response Lwt.t;
-    conn_closed : conn_id -> unit;
+    callback : conn_id -> ?body:string Lwt_stream.t -> Request.request -> response Lwt.t;
+    conn_closed : conn_id -> unit -> unit; 
     port : int;
     root_dir : string option;
     timeout : int option;
   }  
 
-  val respond_string :
-      ?headers:Header.t ->
-      status:Code.status_code ->
-      body:string -> unit -> response Lwt.t
+  val respond_string : ?headers:Header.t -> status:Code.status_code ->
+    body:string -> unit -> response Lwt.t
 
   val respond_error : status:Code.status_code -> body:string -> unit -> response Lwt.t
 
