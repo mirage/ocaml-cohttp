cab4c25fab4e873a5278e3bf90709d99cc5f37d7
Jeremy Yallop <yallop@gmail.com>
2013-03-14 01:49:08 +0000

    Remove make.ml.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 0fe6b2ab6c883acded770f2fa2ed9e86) *)
+(* DO NOT EDIT (digest: 65e3ce6c6b67aa562571610c5665bdf1) *)
 module OASISGettext = struct
-(* # 21 "/auto/homes/jdy22/.opam/4.00.1/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
+(* # 21 "/home/jeremy/.opam/4.01.0dev+trunk/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
 
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-(* # 21 "/auto/homes/jdy22/.opam/4.00.1/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
+(* # 21 "/home/jeremy/.opam/4.01.0dev+trunk/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
 
 
 
@@ -116,7 +116,7 @@ end
 
 # 117 "myocamlbuild.ml"
 module BaseEnvLight = struct
-(* # 21 "/auto/homes/jdy22/.opam/4.00.1/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
+(* # 21 "/home/jeremy/.opam/4.01.0dev+trunk/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
 
   module MapString = Map.Make(String)
 
@@ -214,7 +214,7 @@ end
 
 # 215 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
-(* # 21 "/auto/homes/jdy22/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
+(* # 21 "/home/jeremy/.opam/4.01.0dev+trunk/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
 
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -323,7 +323,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-(* # 21 "/auto/homes/jdy22/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 21 "/home/jeremy/.opam/4.01.0dev+trunk/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -339,7 +339,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
 
-(* # 56 "/auto/homes/jdy22/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 56 "/home/jeremy/.opam/4.01.0dev+trunk/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   type t =
       {
