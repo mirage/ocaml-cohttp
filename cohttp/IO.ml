91dcef803c4c761273d1998751193c14a6c12c48
Jeremy Yallop <yallop@gmail.com>
2013-03-14 01:47:57 +0000

    Dedicated module for IO signature.

---
+++ cohttp/IO.ml
@@ -0,0 +1,15 @@
+module type S = sig
+  type +'a t
+  val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+  val return : 'a -> 'a t
+
+  type ic
+  type oc
+
+  val iter : ('a -> unit t) -> 'a list -> unit t
+  val read_line : ic -> string option t
+  val read : ic -> int -> string t
+  val read_exactly : ic -> string -> int -> int -> bool t
+
+  val write : oc -> string -> unit t
+end
