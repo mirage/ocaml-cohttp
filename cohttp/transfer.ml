mirage/ocaml-cohttp

commit 52cdf69c6de30b08e6a3216b151107b26140b520

An unknown transfer encoding means that there could be a body (it just has to be read until EOF)
Add test case for this too.

Closes #35

cohttp/transfer.ml
@@ -35,6 +35,6 @@ let has_body =
   function
   | Chunked -> true
   | Fixed 0 -> false
-  | Unknown -> false
+  | Unknown -> true
   | Fixed _ -> true
 

lib_test/test_net_lwt_lastminute.ml
@@ -4,6 +4,7 @@ open Printf
 let u1 = "http://www.lastminute.com"
 let u2 = "http://www.bbc.co.uk"
 let u3 = "http://twitter.com"
+let u4 = "http://www.rustorka.com:2710/announce?info_hash=/00%00%00%00%00%00%0000%000%00%00%00%00%00%000&peer_id=00000000000000000000"
 
 (* a simple function to access the content of the response *)
 let content = function
@@ -13,7 +14,7 @@ let content = function
 
 (* launch both requests in parallel *)
 let t =
-  let uris = List.map Uri.of_string [u1;u2;u3] in
+  let uris = List.map Uri.of_string [u1;u2;u3;u4] in
   Lwt_list.map_p (fun uri ->
     printf "fetching %s\n%!" (Uri.to_string uri);
     Cohttp_lwt_unix.Client.get uri

commit e9c824e6a2e2f1b57a550cc86ee1f45b1a1af6bf

Fix transfer length detection (regression from 52cdf69c6de30b08e6a3216b151107b26140b520)
This was affecting requests as well as responses.
Fixes #42

cohttp/transfer.ml
@@ -35,6 +35,6 @@ let has_body =
   function
   | Chunked -> true
   | Fixed 0 -> false
-  | Unknown -> true
+  | Unknown -> false
   | Fixed _ -> true
 