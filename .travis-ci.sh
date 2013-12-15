8400e387f6c4197354e72aea7181e2c5a907449b
Anil Madhavapeddy <anil@recoil.org>
2013-12-15 11:49:22 +0000

    Travis: Since the Forge is down, switch to the OPAM mirror in test

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -20,7 +20,8 @@ echo OPAM versions
 opam --version
 opam --git-version
 
-opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
+# opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
+opam init
 opam install ${OPAM_DEPENDS}
 
 eval `opam config env`
