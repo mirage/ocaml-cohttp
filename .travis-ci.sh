54a467dc6aad563f454078253c67683b98a814dd
Anil Madhavapeddy <anil@recoil.org>
2013-12-18 12:42:13 +0000

    Add Travis files

---
+++ .travis-ci.sh
@@ -0,0 +1,28 @@
+OPAM_DEPENDS="mirage-unix cohttp mirage-tcpip-unix"
+
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
+export OPAMYES=1
+export OPAMVERBOSE=1
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+opam init
+depext=`opam install -e ubuntu $OPAM_DEPENDS`
+sudo apt-get install -qq $depext
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+make
