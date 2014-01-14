e505dc6aab894b151dba298c7d11a14953a3e6a9
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-13 17:03:10 -0800

    sexpify Request.t
    Use the uri wrapper in the type definition

--- cohttp/request.ml
+++ cohttp/request.ml
@@ -15,13 +15,15 @@
  *
  *)
 
+open Sexplib.Std
+
 type t = { 
   mutable headers: Header.t;
   mutable meth: Code.meth;
-  mutable uri: Uri.t;
+  mutable uri: Uri_sexp.t;
   mutable version: Code.version;
   mutable encoding: Transfer.encoding;
-} with fields
+} with fields, sexp
 
 let make ?(meth=`GET) ?(version=`HTTP_1_1) ?encoding ?headers uri =
   let headers = 
