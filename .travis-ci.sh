a7710f1c4dfd49457a828a040e2669be8c2e54ff
Anil Madhavapeddy <anil@recoil.org>
2014-05-31 17:35:29 +0100

    travis: +ipaddr

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="lwt stringext ssl uri re conduit sexplib fieldslib"
+OPAM_DEPENDS="lwt stringext ssl uri re conduit sexplib fieldslib ipaddr"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
