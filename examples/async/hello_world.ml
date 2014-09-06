f95245a355fe6033980906e4e3898ab60f019049
Jon Loldrup <loldrup@gmail.com>
2014-09-06 22:49:20 +0200

    Improved /examples/async/hello_world.ml for newbies
    As I am trying to grasp the cohttp library I am studying the hello_world.ml carefully.
    I find there are a bunch of names (presumeably mostly module names) that I struggle to find the definitions of. Therefor I have added some explicit references to the code, and also I have removed the 'open' directives for clarity.
    I hope you will accept my changes for the benefit of other newbies studying your cohttp library.

--- examples/async/hello_world.ml
+++ examples/async/hello_world.ml
@@ -1,37 +1,64 @@
 (* This file is in the public domain *)
 
-open Core.Std
-open Async.Std
-open Cohttp_async
+(* open Core.Std *)
+(* open Async.Std *)
+(* open Cohttp_async *)
  
 (* given filename: hello_world.ml compile with: 
    $ corebuild hello_world.native -pkg cohttp.async
 *)
  
 let handler ~body:_ _sock req =
-  let uri = Cohttp.Request.uri req in
-  match Uri.path uri with 
+  let uri = Cohttp.Request.uri req in  (* a *)
+  match Uri.path uri with              (* b *)
   | "/test" ->
        Uri.get_query_param uri "hello"
-    |> Option.map ~f:(fun v -> "hello: " ^ v)
-    |> Option.value ~default:"No param hello supplied"
-    |> Server.respond_with_string
+    |> Core.Std.Option.map ~f:(fun v -> "hello: " ^ v)            (* c *)
+    |> Core.Std.Option.value ~default:"No param hello supplied"
+    |> Cohttp_async.Server.respond_with_string                    (* d *)
   | _ ->
-    Server.respond_with_string ~code:`Not_found "Route not found"
+    Cohttp_async.Server.respond_with_string ~code:`Not_found "Route not found"
  
 let start_server port () =
-  eprintf "Listening for HTTP on port %d\n" port;
-  eprintf "Try 'curl http://localhost:%d/test?hello=xyz'\n%!" port;
+  Async.Std.eprintf "Listening for HTTP on port %d\n" port;       (* e *)
+  Async.Std.eprintf "Try 'curl http://localhost:%d/test?hello=xyz'\n%!" port;
+  let (>>=) = Async.Std.Deferred.(>>=) in                         (* f *)
   Cohttp_async.Server.create ~on_handler_error:`Raise
-    (Tcp.on_port port) handler
-  >>= fun _ -> Deferred.never ()
+    (Async.Std.Tcp.on_port port) handler                          (* g *)
+  >>= fun _ -> Async.Std.Deferred.never ()
 
 let () =
-  Command.async_basic
+  Async.Std.Command.async_basic                                   (* h *)
     ~summary:"Start a hello world Async server"
-    Command.Spec.(empty +>
-      flag "-p" (optional_with_default 8080 int)
+    Async.Std.Command.Spec.(empty +>                              (* i *)
+      flag "-p" (optional_with_default 8080 int)                  (* j *)
         ~doc:"int Source port to listen on"
     ) start_server
     
-  |> Command.run
+  |> Async.Std.Command.run                                        (* k *)
+
+
+(*  a   ocaml-cohttp/cohttp/request.mli ->
+        ocaml-cohttp/cohttp/s.mli  *)
+
+(*  b   ???  *)
+
+(*  c   core_kernel/lib/option.mli ->
+        core_kernel/lib/monad.ml  *)
+
+(*  d   ocaml-cohttp/async/cohttp_async.mli  *)
+
+(*  e   async_unix/lib/std.ml ->
+        ???  *)
+
+(*  f   ???  *)
+
+(*  g   ???  *)
+
+(*  h   ???  *)
+
+(*  i   ???  *)
+
+(*  j   ???  *)
+
+(*  k   ???  *)
