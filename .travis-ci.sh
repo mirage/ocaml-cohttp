4c69e1f4d475fcebf8d4dce8be61f88f984e8b0d
Anil Madhavapeddy <anil@recoil.org>
2014-09-07 18:20:26 +0100

    Test OCaml 4.02 and new OPAM 1.2 as well

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,10 +1,13 @@
-OPAM_DEPENDS="lwt stringext ssl uri re conduit sexplib fieldslib ipaddr cstruct"
+OPAM_DEPENDS="lwt stringext ssl uri re conduit sexplib fieldslib ipaddr cstruct js_of_oaml"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
 4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
 4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
+4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
