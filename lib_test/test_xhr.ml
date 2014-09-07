e73ff3f4eba9cda5f183347ce4756c5e88924ad1
andrewray <evilkidder@gmail.com>
2014-09-07 18:01:23 +0100

    Remove Yojson.  Display response.

--- lib_test/test_xhr.ml
+++ lib_test/test_xhr.ml
@@ -35,12 +35,19 @@ let get_input n =
     | _ -> failwith ("couldn't find text element" ^ n)
 let value n = Js.to_string (get_input n)##value
 
+(* JSON object used for pretty printing result *)
+type json_object
+class type json = object
+    method parse : Js.js_string Js.t -> json_object Js.t Js.meth
+    method stringify : json_object Js.t -> unit Js.opt -> int -> Js.js_string Js.t Js.meth
+end
+let json : json Js.t = Js.Unsafe.variable "JSON"
+let pretty str = json##stringify(json##parse(Js.string str), Js.null, 2)
+
 let main _ = 
+  let output_response = get_element "output-response" in
   let output_list_repos = get_element "output-list-repos" in
   let list_repos = get_element "list-repos" in
-  
-  (* pretty print output *)
-  let pretty s = Yojson.Basic.(pretty_to_string (from_string s)) in
   
   (* run the cohttp query to github *)
   let run_query _ = 
@@ -48,8 +55,18 @@ let main _ =
       lwt resp, body = Cohttp_lwt_xhr.Client.get 
         Uri.(of_string ("https://api.github.com/users/" ^ value "username" ^ "/repos"))
       in
+      (* show the response data *)
+      let b = Buffer.create 1024 in
+      let add s = Buffer.add_string b s; Buffer.add_string b "\n" in
+      add Cohttp.(Code.(string_of_version resp.Response.version)); 
+      add Cohttp.(Code.(string_of_status resp.Response.status));
+      Cohttp.Header.iter (fun k v -> List.iter (fun v -> add (k ^ ": " ^ v)) v) 
+        resp.Cohttp.Response.headers;
+      output_response##innerHTML <- Js.string (Buffer.contents b);
+
+      (* show the body as pretty printed json *)
       lwt body = Cohttp_lwt_body.to_string body in
-      output_list_repos##innerHTML <- Js.string (pretty body);
+      output_list_repos##innerHTML <- pretty body;
       Lwt.return ());
     Js._false
   in
