ab286429c8e7730f2249ccfd7b53ea05adf0b980
Anil Madhavapeddy <anil@recoil.org>
2014-09-08 11:01:52 +0100

    travis: try an opam update

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -31,6 +31,7 @@ opam --git-version
 
 opam init
 opam remote add mirage-dev git://github.com/mirage/mirage-dev
+opam update
 sudo apt-get install -qq `opam install -e ubuntu ${OPAM_DEPENDS}`
 opam install -v -j 2 core_kernel
 opam install ${OPAM_DEPENDS}
