7e3fdb78013469b526fe8a981a3e34012f2d6b47
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-10-30 21:49:22 -0400

    Add simple POST body example for async
    As asked by @loldrup

---
+++ examples/async/receive_post.ml
@@ -0,0 +1,29 @@
+(* This file is in the public domain *)
+open Core.Std
+open Async.Std
+open Cohttp_async
+
+(* compile with: $ corebuild receive_post.native -pkg cohttp.async *)
+
+let start_server port () =
+  eprintf "Listening for HTTP on port %d\n" port;
+  eprintf "Try 'curl -X POST -d 'foo bar' http://localhost:%d\n" port;
+  Cohttp_async.Server.create ~on_handler_error:`Raise
+    (Tcp.on_port port) (fun ~body _ req ->
+      match req |> Cohttp.Request.meth with
+      | `POST ->
+        (Body.to_string body) >>= (fun body ->
+          Log.Global.info "Body: %s" body;
+          Server.respond `OK)
+      | _ -> Server.respond `Method_not_allowed
+    )
+  >>= fun _ -> Deferred.never ()
+
+let () =
+  Command.async_basic
+    ~summary:"Simple http server that ouputs body of POST's"
+    Command.Spec.(empty +>
+                  flag "-p" (optional_with_default 8080 int)
+                    ~doc:"int Source port to listen on"
+                 ) start_server
+  |> Command.run
