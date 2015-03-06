79105039acb3383d471b360162f26b9df576ca7b
Leo <leowzukw@vmail.me>
2015-03-06 10:13:27 +0100

    Fix typo
    Forgotten a ' in examples/async/receive_post.ml

--- examples/async/receive_post.ml
+++ examples/async/receive_post.ml
@@ -7,7 +7,7 @@ open Cohttp_async
 
 let start_server port () =
   eprintf "Listening for HTTP on port %d\n" port;
-  eprintf "Try 'curl -X POST -d 'foo bar' http://localhost:%d\n" port;
+  eprintf "Try 'curl -X POST -d 'foo bar' http://localhost:%d'\n" port;
   Cohttp_async.Server.create ~on_handler_error:`Raise
     (Tcp.on_port port) (fun ~body _ req ->
       match req |> Cohttp.Request.meth with
