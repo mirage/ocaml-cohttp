c429f5ea47b2792a4c3e2136a6ca8c3557031c9c
David Sheets <sheets@alum.mit.edu>
2015-04-09 11:11:31 +0100

    Add some request parsing tests
    These seem to trigger an assertion failure in OCaml 4.01.0, 4.02.1, and 4.03.0+trunk

--- lib_test/test_request.ml
+++ lib_test/test_request.ml
@@ -2,6 +2,8 @@ open OUnit
 open Printf
 open Cohttp
 
+module StringRequest = Request.Make(String_io.M)
+
 let uri_userinfo = Uri.of_string "http://foo:bar@ocaml.org"
 
 let header_auth =
@@ -32,10 +34,57 @@ let auth_uri _ =
     (r |> Request.headers |> Header.get_authorization)
     (Some (`Basic ("foo", "bar")))
 
+let parse_request_uri_ r uri name =
+  String_io.M.(
+    StringRequest.read (String_io.open_in r)
+    >>= function
+    | `Ok pr ->
+      assert_equal pr.Request.uri uri
+    | _ -> assert_failure (name^" parse failed")
+  )
+
+let parse_request_uri _ =
+  let r = "GET / HTTP/1.1\r\n\r\n" in
+  let uri = Uri.of_string "/" in
+  parse_request_uri_ r uri "parse_request_uri"
+
+let parse_request_uri_double_slash _ =
+  let r = "GET // HTTP/1.1\r\n\r\n" in
+  let uri = Uri.with_path (Uri.of_string "") "//" in
+  parse_request_uri_ r uri "parse_request_uri_double_slash"
+
+let parse_request_uri_triple_slash _ =
+  let r = "GET /// HTTP/1.1\r\n\r\n" in
+  let uri = Uri.with_path (Uri.of_string "") "///" in
+  parse_request_uri_ r uri "parse_request_uri_triple_slash"
+
+let parse_request_uri_host _ =
+  let r = "GET / HTTP/1.1\r\nHost: example.com\r\n\r\n" in
+  let uri = Uri.of_string "//example.com/" in
+  parse_request_uri_ r uri "parse_request_uri_host"
+
+let parse_request_uri_host_double_slash _ =
+  let r = "GET // HTTP/1.1\r\nHost: example.com\r\n\r\n" in
+  let uri = Uri.of_string "//example.com//" in
+  parse_request_uri_ r uri "parse_request_uri_host_double_slash"
+
+let parse_request_uri_host_triple_slash _ =
+  let r = "GET /// HTTP/1.1\r\nHost: example.com\r\n\r\n" in
+  let uri = Uri.of_string "//example.com///" in
+  parse_request_uri_ r uri "parse_request_uri_host_triple_slash"
+
 let _ =
   ("Request" >:::
    [ "Test header has auth" >:: header_has_auth
    ; "Test uri has user info" >:: uri_has_userinfo
    ; "Auth from Uri - do not override" >:: auth_uri_no_override
    ; "Auth from Uri" >:: auth_uri
+   ; "Parse simple request URI" >:: parse_request_uri
+   ; "Parse request URI double slash" >:: parse_request_uri_double_slash
+   ; "Parse request URI triple slash" >:: parse_request_uri_triple_slash
+   ; "Parse request URI with host" >:: parse_request_uri_host
+   ; "Parse request URI double slash with host"
+     >:: parse_request_uri_host_double_slash
+   ; "Parse request URI triple slash with host"
+     >:: parse_request_uri_host_triple_slash
    ]) |> run_test_tt_main
