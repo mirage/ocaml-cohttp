8ad05cd1a9748f8e0fa89c6594fe2f296fc41c83
Rudi Grinberg <rudi.grinberg@gmail.com>
2015-06-04 01:47:54 -0400

    Move cohttp_async.body into own module

--- async/cohttp_async.mli
+++ async/cohttp_async.mli
@@ -34,21 +34,7 @@ module Response : sig
   include Cohttp.S.Http_io with type t := Cohttp.Response.t and module IO=IO
 end
 
-module Body : sig
-  type t = [
-    | Cohttp.Body.t
-    | `Pipe of string Pipe.Reader.t
-  ] with sexp_of
-  include Cohttp.S.Body with type t := t
-  val drain : t -> unit Deferred.t
-  val is_empty : t -> bool Deferred.t
-  val to_string : t -> string Deferred.t
-  val to_string_list : t -> string list Deferred.t
-  val to_pipe : t -> string Pipe.Reader.t
-  val of_pipe : string Pipe.Reader.t -> t
-  val map : t -> f:(string -> string) -> t
-  val as_pipe : t -> f:(string Pipe.Reader.t -> string Pipe.Reader.t) -> t
-end
+module Body : (module type of Cohttp_async_body)
 
 module Client : sig
 
