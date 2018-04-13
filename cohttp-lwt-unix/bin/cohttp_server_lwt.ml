cc80e63f4b2835cf5bd7971613a2e397f9dbb497
Raphaël Proust <code@bnwr.net>
2018-04-13 09:52:39 +0800

    Set Lwt-aware reporter in cohttp-lwt-unix/bin

--- cohttp-lwt-unix/bin/cohttp_server_lwt.ml
+++ cohttp-lwt-unix/bin/cohttp_server_lwt.ml
@@ -117,9 +117,35 @@ let start_server docroot port host index tls () =
   let ctx = Cohttp_lwt_unix.Net.init ~ctx () in
   Server.create ~ctx ~mode config
 
+(* The example of Lwt-aware reporter in Logs' documentation *)
+let lwt_reporter () =
+  let buf_fmt ~like =
+    let b = Buffer.create 512 in
+    Fmt.with_buffer ~like b,
+    fun () -> let m = Buffer.contents b in Buffer.reset b; m
+  in
+  let app, app_flush = buf_fmt ~like:Fmt.stdout in
+  let dst, dst_flush = buf_fmt ~like:Fmt.stderr in
+  let reporter = Logs_fmt.reporter ~app ~dst () in
+  let report src level ~over k msgf =
+    let k () =
+      let write () = match level with
+      | Logs.App -> Lwt_io.write Lwt_io.stdout (app_flush ())
+      | _ -> Lwt_io.write Lwt_io.stderr (dst_flush ())
+      in
+      let unblock () = over (); Lwt.return_unit in
+      Lwt.finalize write unblock |> Lwt.ignore_result;
+      k ()
+    in
+    reporter.Logs.report src level ~over:(fun () -> ()) k msgf;
+  in
+  { Logs.report = report }
+
 let lwt_start_server docroot port host index verbose tls =
   Logs.set_level verbose;
+  Logs.set_reporter (lwt_reporter ());
   Lwt_main.run (start_server docroot port host index tls ())
+
 
 open Cmdliner
 
