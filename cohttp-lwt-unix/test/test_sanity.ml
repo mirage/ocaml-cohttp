c4537f421b9d3b74a6a54be747afbf234ca3b949
Marcello Seri <marcello.seri@gmail.com>
2020-09-02 16:39:48 +0200

    cohttp_lwt_unix/test: move noisy test apart and reduce logging to remove CI spurious errors
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- cohttp-lwt-unix/test/test_sanity.ml
+++ cohttp-lwt-unix/test/test_sanity.ml
@@ -1,6 +1,5 @@
 open Lwt.Infix
 open OUnit
-open Cohttp
 open Cohttp_lwt_unix
 open Cohttp_lwt_unix_test
 
@@ -19,21 +18,24 @@ let chunk_body = ["one"; ""; " "; "bar"; ""]
 let leak_repeat = 1024
 
 let () = Debug.activate_debug ()
-let () = Logs.set_level (Some Info)
+let () = Logs.set_level (Some Warning)
 
 let cond = Lwt_condition.create ()
+
+let check_logs test () =
+  let old = Logs.(warn_count () + err_count ()) in
+  test () >|= fun () ->
+  let new_errs = Logs.(warn_count () + err_count ()) - old in
+  if new_errs > 0 then
+    Fmt.failwith "Test produced %d log messages at level >= warn" new_errs
 
 let server =
   List.map const [ (* t *)
     Server.respond_string ~status:`OK ~body:message ();
-    (* empty_chunk *)
-    Server.respond ~status:`OK ~body:(Body.of_string_list chunk_body) ();
     (* pipelined_chunk *)
     Server.respond ~status:`OK ~body:(Body.of_string "") ();
     Server.respond ~status:`OK ~body:(Body.of_string_list chunk_body) ();
     Server.respond ~status:`OK ~body:(Body.of_string "") ();
-    (* not modified *)
-    Server.respond ~status:`Not_modified ~body:Body.empty ();
     (* pipelined_interleave *)
     Server.respond_string ~status:`OK ~body:"one" ();
     Server.respond_string ~status:`OK ~body:"two" ();
@@ -50,12 +52,7 @@ let server =
     end()
   ]
   @
-  (fun _ body -> (* Returns 500 on bad file *)
-     Body.to_string body >>= fun fname ->
-     Server.respond_file ~fname () >|= fun rsp ->
-     `Response rsp
-  )
-  :: (
+  (
     Array.init (leak_repeat * 2) (fun _ _ _ ->
       (* no leaks *)
       Server.respond_string ~status:`OK ~body:"no leak" () >|= fun rsp ->
@@ -98,23 +95,12 @@ let server =
   ]
   |> response_sequence
 
-let check_logs test () =
-  let old = Logs.(warn_count () + err_count ()) in
-  test () >|= fun () ->
-  let new_errs = Logs.(warn_count () + err_count ()) - old in
-  if new_errs > 0 then
-    Fmt.failwith "Test produced %d log messages at level >= warn" new_errs
-
 let ts =
   Cohttp_lwt_unix_test.test_server_s server begin fun uri ->
     let t () =
       Client.get uri >>= fun (_, body) ->
       body |> Body.to_string >|= fun body ->
       assert_equal body message in
-    let empty_chunk () =
-      Client.get uri >>= fun (_, body) ->
-      body |> Body.to_string >|= fun body ->
-      assert_equal body (String.concat "" chunk_body) in
     let pipelined_chunk () =
       let printer x = x in
       let body = String.concat "" chunk_body in
@@ -135,14 +121,6 @@ let ts =
         ) resps >>= fun () ->
       assert_equal ~printer:string_of_int 3 !counter;
       Lwt.return_unit in
-    let not_modified_has_no_body () =
-      Client.get uri >>= fun (resp, body) ->
-      assert_equal (Response.status resp) `Not_modified;
-      let headers = Response.headers resp in
-      assert_equal ~printer:Transfer.string_of_encoding
-        Transfer.Unknown (Header.get_transfer_encoding headers);
-      body |> Body.is_empty >|= fun is_empty ->
-      assert_bool "No body returned when not modified" is_empty in
     let pipelined_interleave () =
       let r n =
         let uri = Uri.with_query' uri ["test", (string_of_int n)] in
@@ -173,23 +151,6 @@ let ts =
       Client.get uri >>= fun (_resp, body) ->
       Body.to_string body >|= fun body ->
       assert_equal ~printer:string_of_int (1000 * 64) (String.length body) in
-    let unreadable_file_500 () =
-      let fname = "unreadable500" in
-      Lwt.finalize (fun () ->
-          Lwt_io.open_file ~flags:[Lwt_unix.O_CREAT] ~perm:0o006
-            ~mode:Lwt_io.Output fname >>= fun oc ->
-          Lwt_io.write_line oc "never read" >>= fun () ->
-          Lwt_io.close oc >>= fun () ->
-          Client.post uri ~body:(Body.of_string fname)
-          >>= begin fun (resp, body) ->
-            assert_equal ~printer:Code.string_of_status
-              (Response.status resp) `Internal_server_error;
-            Body.to_string body
-          end >|= fun body ->
-          assert_equal ~printer:(fun x -> "'" ^ x ^ "'")
-            body "Error: Internal Server Error"
-        ) (fun () -> Lwt_unix.unlink fname)
-    in
     let test_no_leak () =
       let stream = Array.init leak_repeat (fun _ -> uri) |> Lwt_stream.of_array in
       Lwt_stream.fold_s (fun uri () ->
@@ -222,17 +183,13 @@ let ts =
         Lwt.pause ()
     in
     [ "sanity test",                            check_logs t
-    ; "empty chunk test",                       check_logs empty_chunk
     ; "pipelined chunk test",                   check_logs pipelined_chunk
-    ; "no body when response is not modified",  check_logs not_modified_has_no_body
     ; "pipelined with interleaving requests",   check_logs pipelined_interleave
     ; "massive chunked",                        check_logs massive_chunked
-    ; "unreadable file returns 500",            unreadable_file_500
     ; "no leaks on requests",                   check_logs test_no_leak
     ; "expert response",                        check_logs expert_pipelined
     ; "client_close",                           check_logs client_close
     ]
   end
-
 
 let _ = ts |> run_async_tests |> Lwt_main.run
