bf2b48e6920a836352aadbc1c3bf5c7604375eea
Thomas Gazagnaire <thomas@gazagnaire.org>
2021-02-06 19:00:31 +0100

    Adapt to conduit-mirage 2.3.0

--- cohttp-mirage/src/server_with_conduit.mli
+++
@@ -1,5 +0,0 @@
-include Cohttp_lwt.S.Server with type IO.conn = Conduit_mirage.Flow.flow
-(** HTTP server with conduit. *)
-
-val connect :
-  Conduit_mirage.t -> (Conduit_mirage.server -> t -> unit Lwt.t) Lwt.t
