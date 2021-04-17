0838e35582360617e9d9ae7957e1a1cfd9b22674
Marcello Seri <marcello.seri@gmail.com>
2021-04-17 13:34:20 +0200

    lwt: improve docstrings
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- cohttp-lwt/src/server.mli
+++ cohttp-lwt/src/server.mli
@@ -2,7 +2,6 @@
     requests down a connection that is established by the user. The resulting
     module satisfies the {!Server} module type.
 
-    The {!Logs} source name for this module's logger is ["cohttp.lwt.server"].
-    Refer to the {!Debug} module for further details.*)
+    The {!Logs} source name for this module's logger is ["cohttp.lwt.server"].*)
 
 module Make (IO : S.IO) : S.Server with module IO = IO
