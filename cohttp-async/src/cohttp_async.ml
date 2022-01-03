01ac73dba84b05b832d4384a1afc694df47fa168
Rudi Grinberg <me@rgrinberg.com>
2022-01-02 17:19:10 -0700

    refactor: deprecate Cohttp_async.Io
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: F2588212-F30F-4ED1-AE2B-FE45DB81D248

--- cohttp-async/src/cohttp_async.ml
+++ cohttp-async/src/cohttp_async.ml
@@ -1,7 +1,7 @@
 module Body = Body
 module Body_raw = Body_raw [@@deprecated "Use Body"]
 module Client = Client
-module Io = Io
+module Io = Io [@@deprecated "This module is not for public consumption"]
 module Request = Cohttp.Request [@@deprecated "Use Cohttp.Request directly"]
 module Response = Cohttp.Response [@@deprecated "Use Cohttp.Response directly"]
 module Server = Server
