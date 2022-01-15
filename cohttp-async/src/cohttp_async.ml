a19cbc9719b07cd82b2d308329ff8d53e1cfc3b5
Anurag Soni <anurag@sonianurag.com>
2022-01-14 22:11:22 -0500

    add IO modules to public api
    This will be the only way to perform any IO operations when working with
    expert responses, so we should probably include it in the public api.

--- cohttp-async/src/cohttp_async.ml
+++ cohttp-async/src/cohttp_async.ml
@@ -1,7 +1,8 @@
 module Body = Body
 module Body_raw = Body [@@deprecated "Use Body"]
 module Client = Client
-module Io = Io [@@deprecated "This module is not for public consumption"]
+module Io = Io.IO
+module Input_channel = Input_channel
 module Request = Cohttp.Request [@@deprecated "Use Cohttp.Request directly"]
 module Response = Cohttp.Response [@@deprecated "Use Cohttp.Response directly"]
 module Server = Server
