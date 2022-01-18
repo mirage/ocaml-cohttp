32a62f7ca064d8c20840796f20a6bc2dffedd7aa
Rudi Grinberg <me@rgrinberg.com>
2022-01-18 00:00:32 -0700

    simplify server read
    ps-id: 52688A2C-CF3D-458B-9D1B-25FC03CB8456

--- cohttp-server-lwt-unix/src/cohttp_server_lwt_unix.ml
+++ cohttp-server-lwt-unix/src/cohttp_server_lwt_unix.ml
@@ -98,11 +98,7 @@ module Input_channel = struct
   type t = { buf : Bytebuffer.t; chan : Lwt_io.input_channel }
 
   let refill ic buf ~pos ~len =
-    Lwt.catch
-      (fun () ->
-        Lwt_io.read_into ic buf pos len >|= fun c ->
-        if c > 0 then `Ok c else `Eof)
-      (function Lwt_io.Channel_closed _ -> Lwt.return `Eof | exn -> raise exn)
+    Lwt_io.read_into ic buf pos len >|= fun c -> if c > 0 then `Ok c else `Eof
 
   let create ?(buf_len = 0x4000) chan =
     { buf = Bytebuffer.create buf_len; chan }
