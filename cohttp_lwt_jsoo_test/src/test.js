2c17fc42d716b2e7a51168c0ed587870b2b3667f
Sora Morimoto <sora@morimoto.io>
2022-01-22 15:53:43 +0900

    Enable js-test
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- cohttp_lwt_jsoo_test/src/test.js
+++ cohttp_lwt_jsoo_test/src/test.js
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 
-const assert = require('assert');
+const assert = require("assert");
+const { XMLHttpRequest } = require("xmlhttprequest");
 
-global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const tests = require('./cohttp_lwt_jsoo_test.bc.js')
+global.XMLHttpRequest = XMLHttpRequest;
+
+const tests = require("./cohttp_lwt_jsoo_test.bc.js");
 
 async function main() {
   {
-    const [status, body] = await tests.request("https://mirage.io");
-    assert(status == 200);
+    const [status, _body] = await tests.request("https://mirage.io");
+    assert(status === 200);
   }
   {
-    const [status, body] = await tests.request("https://this.domain.does.not.exist");
-    assert(status == 0);
+    const [status, _body] = await tests.request(
+      "https://this.domain.does.not.exist"
+    );
+    assert(status === 0);
   }
 }
 
-main()
+main();
