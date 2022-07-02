c37dd5f43be86d7ce1a78a59e62af6a28c411881
Rudi Grinberg <me@rgrinberg.com>
2022-07-02 12:58:30 -0500

    test(cohttp-lwt): demonstrate infinite loop
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: a4b9b6b1-5944-49c8-b840-cca95f16c8a9

---
+++ cohttp-lwt/test/bytebuffer_tests.ml
@@ -0,0 +1,37 @@
+module Bytebuffer = Cohttp_lwt.Private.Bytebuffer
+
+let%expect_test "read" =
+  let line = "foobar\n" in
+  let test buf_size =
+    let buf = Bytebuffer.create buf_size in
+    let refill =
+      let line_pos = ref 0 in
+      let reads_left = ref 10 in
+      fun buf ~pos ~len ->
+        if !reads_left = 0 then raise Exit;
+        decr reads_left;
+        let available = String.length line - !line_pos in
+        if available = 0 then Lwt.return `Eof
+        else
+          let read_len = min len available in
+          BytesLabels.blit_string ~src:line ~src_pos:!line_pos ~dst:buf
+            ~dst_pos:pos ~len:read_len;
+          line_pos := !line_pos + read_len;
+          Lwt.return (`Ok read_len)
+    in
+    let open Lwt.Syntax in
+    Lwt_main.run
+    @@ Lwt.catch
+         (fun () ->
+           let+ res = Bytebuffer.read_line buf refill in
+           match res with
+           | None -> print_endline "failed to read line"
+           | Some line -> Printf.printf "read line: %S\n" line)
+         (function
+           | Exit -> Lwt.return @@ print_endline "failed to read"
+           | _ -> assert false)
+  in
+  test (String.length line);
+  [%expect {| read line: "foobar" |}];
+  test (String.length line - 1);
+  [%expect {| failed to read |}]
