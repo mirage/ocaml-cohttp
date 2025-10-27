d884c057d55bdb9415695e2ad1efbc1bfb80a5ba
Shon Feder <shon.feder@gmail.com>
2025-10-26 22:44:07 -0400

    Add a connection abstraction
    We aim to cache connections. But what do we count as a connection?
    
    When making requests, `Eio.Buf_write.with_flow socket f` opens the
    `socket` for writing, and takes care of ensuring that the flow is closed
    as a finalization after `f` completes. While we have the socket open for
    writing, we also open the `socket` for reading. This is our
    bidirectional connection, but it is only kept open as long as `f` runs.
    
    To cache this open socket, but make it possible to reuse it for multiple
    requests, we do the following:
    
    - We make each connection a forked process that can read from a channel
      (an `Eio.Stream`).
      - Each message sent on the stream includes a resolver for a promise,
        which is how the response is returned.
    - The process itself loops on reads from the channel, until the channel
      is closed.
    - Since each processes is forked
      - it will get cleaned up when the enclosing switch is cancelled
      - but we can also close all the channels to tell them to shut down
    
    This is all abstracted into the new `Connection` API (echoing the same
    API in cohttp-lwt).
    
    Our cache will then be a map `Socketaddr -> Connection.t`.

--- cohttp-eio/src/connection_cache.ml
+++ cohttp-eio/src/connection_cache.ml
@@ -1,48 +1,111 @@
 (* TODO open Eio.Std *)
 open Utils
 
-let call_on_socket ~sw ~headers ~body ~chunked ~absolute_form meth uri socket =
+module Connection : sig
+  type t
+  val create : ?limit:int -> sw:Eio.Switch.t -> S.connection -> t
+  (** TODO(doc): Create a new connection *)
+
+  val close : t -> unit
+  (** TODO(doc): Close the connection *)
+
+  (* val send : ?body:Body.t -> request:Http.Request.t -> t -> (Http.Response.t * Body.t) *)
+  (** TODO(doc): Send a request and wait for the response *)
+
+  val call : headers:Http.Header.t option ->
+             body:Body.t option ->
+             chunked:bool ->
+             absolute_form:bool option ->
+             Cohttp.Code.meth ->
+             Uri.t ->
+             t ->
+             Http.Response.t * Body.t
+  (** TODO(doc): Form and send a request, and wait for the response *)
+end = struct
+
+  type req = {
+      body:Body.t option;
+      request: Http.Request.t;
+      resolver: (Http.Response.t * Body.t) Eio.Promise.u
+    }
+
+  (* A connection is a forked process that reads from a stream of messages.
+     its principle type a function that lets users send messages along the stream.  *)
+  type t = req option -> unit
+
+  (** TODO: Doc
+
+      - Send [None] to terminate the process *)
+  let create ?(limit=max_int) ~sw socket : t =
+    let request_stream : req option Eio.Stream.t = Eio.Stream.create limit in
+    (* By giving constructors a send function, we provide a write-only stream, 
+       making it impossible for other parts of the program to steal from our stream *)
+    let send_request req = Eio.Stream.add request_stream req in
+    let () =
+      Eio.Buf_write.with_flow socket begin fun output ->
+        let loop () =
+          match Eio.Stream.take request_stream with
+          | None -> () (* Stream "closed", so we terminate *)
+          | Some {request; body; resolver} ->
+             let () =
+               Io.Request.write ~flush:false
+                 (fun writer ->
+                   match body with
+                   | None -> ()
+                   | Some body -> flow_to_writer body writer Io.Request.write_body)
+                 request output
+             in
+             let input = Eio.Buf_read.of_flow ~max_size:max_int socket in
+             match Io.Response.read input with
+             | `Eof -> failwith "connection closed by peer"
+             | `Invalid reason -> failwith reason
+             | `Ok response ->
+                let response = match Cohttp.Response.has_body response with
+                  | `No -> (response, Eio.Flow.string_source "")
+                  | `Yes | `Unknown ->
+                     let body =
+                       let reader = Io.Response.make_body_reader response input in
+                       flow_of_reader (fun () -> Io.Response.read_body_chunk reader)
+                     in
+                     (response, body)
+                in
+                Eio.Promise.resolve resolver response
+        in
+        (* TODO: should we instead fork this loop from outside? *)
+        Eio.Fiber.fork ~sw loop
+        end
+    in
+    send_request
+
+  let close (t : t) = t None 
+
+  let send ?body ~request (t : t) : (Http.Response.t * Body.t) Eio.Promise.t =
+    let promise, resolver = Eio.Promise.create () in
+    t (Some {body; request; resolver});
+    promise
+
+  let call ~headers ~body ~chunked ~absolute_form meth uri conn =
     let body_length =
       if chunked then None
       else
         match body with
         | None -> Some 0L
         | Some (Eio.Resource.T (body, ops)) ->
-            let module X = (val Eio.Resource.get ops Eio.Flow.Pi.Source) in
-            List.find_map
-              (function
-                | Body.String m -> Some (String.length (m body) |> Int64.of_int)
-                | _ -> None)
-              X.read_methods
+           let module X = (val Eio.Resource.get ops Eio.Flow.Pi.Source) in
+           List.find_map
+             (function
+              | Body.String m -> Some (String.length (m body) |> Int64.of_int)
+              | _ -> None)
+             X.read_methods
     in
     let request =
       Cohttp.Request.make_for_client ?headers
         ~chunked:(Option.is_none body_length)
         ?body_length ?absolute_form meth uri
     in
-    Eio.Buf_write.with_flow socket @@ fun output ->
-    let () =
-      Eio.Fiber.fork ~sw @@ fun () ->
-      Io.Request.write ~flush:false
-        (fun writer ->
-          match body with
-          | None -> ()
-          | Some body -> flow_to_writer body writer Io.Request.write_body)
-        request output
-    in
-    let input = Eio.Buf_read.of_flow ~max_size:max_int socket in
-    match Io.Response.read input with
-    | `Eof -> failwith "connection closed by peer"
-    | `Invalid reason -> failwith reason
-    | `Ok response -> (
-        match Cohttp.Response.has_body response with
-        | `No -> (response, Eio.Flow.string_source "")
-        | `Yes | `Unknown ->
-            let body =
-              let reader = Io.Response.make_body_reader response input in
-              flow_of_reader (fun () -> Io.Response.read_body_chunk reader)
-            in
-            (response, body))
+    Eio.Promise.await @@ send ?body ~request conn
+end
+
 
 module No_cache = struct
   type t = unit
@@ -50,31 +113,36 @@ module No_cache = struct
   let call () : S.cache_call =
    fun t ~sw ?headers ?body ?(chunked = false) ?absolute_form meth uri ->
     let _addr, socket = t ~sw uri in
-    call_on_socket ~sw ~headers ~body ~chunked ~absolute_form meth uri socket
+    let conn = Connection.create ~sw socket in
+    let resp = Connection.call ~headers ~body ~chunked ~absolute_form meth uri conn in
+    Connection.close conn;
+    resp
 
   let create () = ()
 end
 
 module Cache = struct
-  module M = struct
-    type 'v t = {
+  module Tbl = struct
+    type t = {
       (* We only cache for [stream] sockets, because we only care for what we can connect to via [Eio.Net.connect] *)
-      hashtbl : (Eio.Net.Sockaddr.stream, 'v) Hashtbl.t;
+      hashtbl : (Eio.Net.Sockaddr.stream, Connection.t) Hashtbl.t;
       mutex : Eio.Mutex.t;
     }
 
     let get k t =
-      Eio.Mutex.use_ro t.mutex (fun () -> Hashtbl.find_all k t.hashtbl)
+      Eio.Mutex.use_ro t.mutex (fun () -> Hashtbl.find_opt k t.hashtbl)
+
+    (* let create limit = *)
   end
 
-  type t = { cache : [ `TODO ] M.t }
+  type t = { cache : Tbl.t }
 
   (* let get_connection t endpoint = *)
   (*   match lookup_in_map endpoint with *)
   (*   | Some c -> c *)
   (*   | None _ -> add_to_mapp endpoint *)
   let _f (t : t) =
-    let _ = M.get in
+    let _ = Tbl.get in
     t.cache
 
   let create = raise (Failure "TODO")
